9aa0ec170570d4cf52874d8af1930d68cb5182b8
Rody Davis <rody.davis.jr@gmail.com>
2019-04-23 17:39:07 -0400

    Refactor

--- flutter-desktop-embedding/project/test/widget_test.dart
+++ flutter-desktop-embedding/project/test/widget_test.dart
@@ -7,7 +7,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:example_flutter/main.dart';
+import 'package:project/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (tester) async {
