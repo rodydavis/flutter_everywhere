e94a172c60257bc71c9c48df3350c008013f232f
Rody Davis <rody.davis.jr@gmail.com>
2019-05-08 09:38:52 -0400

    Updating Desktop/

---
+++ build/aot/arm64/gen_snapshot.d
@@ -0,0 +1,1 @@
+gen_snapshot.d: /Users/rodydavis/Development/flutter/bin/cache/artifacts/engine/ios-release/gen_snapshot
---
+++ build/macos/Build/Intermediates.noindex/Runner.build/Debug/Build Flutter Bundle.build/Script-33CC111E2044C6BF0003C045.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+"$FLUTTER_ROOT"/packages/flutter_tools/bin/macos_build_flutter_assets.sh
+
