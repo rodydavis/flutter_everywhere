ac7d229adbef281bc364457e8fa4c3cd8e6d66c0
Rody Davis <rody.davis.jr@gmail.com>
2019-10-22 11:24:14 -0400

    unified desktop and web

--- core/lib/src/index.dart
+++
@@ -1,4 +0,0 @@
-export 'utils/index.dart';
-export 'classes/index.dart';
-export 'abstract/index.dart';
-export 'constants/index.dart';
--- website/lib/src/index.dart
+++
@@ -1,2 +0,0 @@
-export 'ui/index.dart';
-export 'data/index.dart';
